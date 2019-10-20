25316633da42c75012343061d51adc775809728f
Corey Petty <corey@status.im>
2019-10-20 17:25:09 -0400

    added tokens

---
+++ alethio/tokens.go
@@ -0,0 +1,166 @@
+package alethio
+
+import (
+	"context"
+	"fmt"
+)
+
+type TokensService service
+
+type TokenDetails struct {
+	Data struct {
+		Type       string `json:"type"`
+		ID         string `json:"id"`
+		Attributes struct {
+			Decimals    int      `json:"decimals"`
+			Name        string   `json:"name"`
+			Symbol      string   `json:"symbol"`
+			TokenTypes  []string `json:"tokenTypes"`
+			TotalSupply string   `json:"totalSupply"`
+		} `json:"attributes"`
+		Relationships struct {
+			Contract struct {
+				Data struct {
+					Type string `json:"type"`
+					ID   string `json:"id"`
+				} `json:"data"`
+				Links struct {
+					Related string `json:"related"`
+				} `json:"links"`
+			} `json:"contract"`
+			TokenTransfers struct {
+				Links struct {
+					Related string `json:"related"`
+				} `json:"links"`
+			} `json:"tokenTransfers"`
+		} `json:"relationships"`
+		Links struct {
+			Self string `json:"self"`
+		} `json:"links"`
+	} `json:"data"`
+	Meta struct {
+		LatestBlock struct {
+			Number            int    `json:"number"`
+			BlockCreationTime int    `json:"blockCreationTime"`
+			BlockHash         string `json:"blockHash"`
+		} `json:"latestBlock"`
+	} `json:"meta"`
+}
+
+type TokenTransfers struct {
+	Data []struct {
+		Type       string `json:"type"`
+		ID         string `json:"id"`
+		Attributes struct {
+			BlockCreationTime int    `json:"blockCreationTime"`
+			Cursor            string `json:"cursor"`
+			Decimals          int    `json:"decimals"`
+			GlobalRank        []int  `json:"globalRank"`
+			Symbol            string `json:"symbol"`
+			Value             string `json:"value"`
+		} `json:"attributes"`
+		Relationships struct {
+			ContractMessage struct {
+				Data interface{} `json:"data"`
+			} `json:"contractMessage"`
+			From struct {
+				Data struct {
+					Type string `json:"type"`
+					ID   string `json:"id"`
+				} `json:"data"`
+				Links struct {
+					Related string `json:"related"`
+				} `json:"links"`
+			} `json:"from"`
+			LogEntry struct {
+				Data struct {
+					Type string `json:"type"`
+					ID   string `json:"id"`
+				} `json:"data"`
+				Links struct {
+					Related string `json:"related"`
+				} `json:"links"`
+			} `json:"logEntry"`
+			Originator struct {
+				Data struct {
+					Type string `json:"type"`
+					ID   string `json:"id"`
+				} `json:"data"`
+				Links struct {
+					Related string `json:"related"`
+				} `json:"links"`
+			} `json:"originator"`
+			To struct {
+				Data struct {
+					Type string `json:"type"`
+					ID   string `json:"id"`
+				} `json:"data"`
+				Links struct {
+					Related string `json:"related"`
+				} `json:"links"`
+			} `json:"to"`
+			Token struct {
+				Data struct {
+					Type string `json:"type"`
+					ID   string `json:"id"`
+				} `json:"data"`
+				Links struct {
+					Related string `json:"related"`
+				} `json:"links"`
+			} `json:"token"`
+			Transaction struct {
+				Data struct {
+					Type string `json:"type"`
+					ID   string `json:"id"`
+				} `json:"data"`
+				Links struct {
+					Related string `json:"related"`
+				} `json:"links"`
+			} `json:"transaction"`
+		} `json:"relationships"`
+		Links struct {
+			Self string `json:"self"`
+		} `json:"links"`
+	} `json:"data"`
+	Links NavLinks `json:"links"`
+	Meta  struct {
+		Count       int `json:"count"`
+		LatestBlock struct {
+			Number            int    `json:"number"`
+			BlockCreationTime int    `json:"blockCreationTime"`
+			BlockHash         string `json:"blockHash"`
+		} `json:"latestBlock"`
+		Page struct {
+			HasNext bool `json:"hasNext"`
+			HasPrev bool `json:"hasPrev"`
+		} `json:"page"`
+	} `json:"meta"`
+}
+
+// GetDetails returns the Token details of a given token contract address
+// https://docs.aleth.io/api#tag/Tokens/paths/~1tokens~1{address}/get
+func (s *TokensService) GetDetails(ctx context.Context, address string) (TokenDetails, error) {
+	req, err := s.client.NewRequest("GET", "tokens/"+address, nil)
+	if err != nil {
+		fmt.Print(err)
+		var emptyToken TokenDetails
+		return emptyToken, err
+	}
+	var token TokenDetails
+	_, err = s.client.Do(ctx, req, &token)
+	return token, err
+}
+
+// GetContract returns the Token details of a given token contract address
+// https://docs.aleth.io/api#tag/Tokens/paths/~1tokens~1{address}~1contract/get
+func (s *TokensService) GetContract(ctx context.Context, address string) (TokenTransfers, error) {
+	req, err := s.client.NewRequest("GET", "tokens/"+address+"/transers", nil)
+	if err != nil {
+		fmt.Print(err)
+		var emptyTransfers TokenTransfers
+		return emptyTransfers, err
+	}
+	var transfers TokenTransfers
+	_, err = s.client.Do(ctx, req, &transfers)
+	return transfers, err
+}
